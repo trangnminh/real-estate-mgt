ef1164adc3950af8b34cf0667e6d445fbfe3cd9e
mnskgrayce <mineskgrayce@gmail.com>
2021-11-16 19:45:23 +0700

    init commit

---
+++ src/test/java/eeet2582/realestatemgt/RealEstateMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package eeet2582.realestatemgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
