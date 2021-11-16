cd9c4a5dd0a9b2f3a1f0ed71b6bcf1c6e3f2e673
mnskgrayce <mineskgrayce@gmail.com>
2021-11-16 19:45:23 +0700

    init commit

---
+++ src/main/java/eeet2582/realestatemgt/RealEstateMgtApplication.java
@@ -0,0 +1,13 @@
+package eeet2582.realestatemgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateMgtApplication.class, args);
+	}
+
+}
