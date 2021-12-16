b2b47a33629783afd68f30f3f817b1526bfedf94
mnskgrayce <mineskgrayce@gmail.com>
2021-12-16 23:48:44 +0700

    refactor search with params for user, all endpoints work now

--- src/main/java/eeet2582/realestatemgt/controller/UserController.java
+++ src/main/java/eeet2582/realestatemgt/controller/UserController.java
@@ -3,12 +3,15 @@ package eeet2582.realestatemgt.controller;
 import eeet2582.realestatemgt.model.AppUser;
 import eeet2582.realestatemgt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
+// Implemented: Get all, get one, add, update, delete
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/users")
 public class UserController {
 
     private final UserService userService;
@@ -18,45 +21,42 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    // Just get all users (for debug)
+    @GetMapping("")
     public List<AppUser> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    @GetMapping("/user/{userId}")
+    // Return users matching query with sort, order and pagination
+    // Params aren't mandatory, if not provided will use defaults
+    @GetMapping("/search")
+    public Page<AppUser> getFilteredUsers(@RequestParam(value="query", defaultValue="") String query,
+                                          @RequestParam(value="pageNo", defaultValue="0") int pageNo,
+                                          @RequestParam(value="pageSize", defaultValue="5") int pageSize,
+                                          @RequestParam(value="sortBy", defaultValue="fullName") String sortBy,
+                                          @RequestParam(value="orderBy", defaultValue="asc") String orderBy) {
+        return userService.getFilteredUsers(query, pageNo, pageSize, sortBy, orderBy);
+    }
+
+    // Get one user by ID
+    @GetMapping("/{userId}")
     public AppUser getUserById(@PathVariable("userId") Long userId) {
         return userService.getUserById(userId);
     }
 
-    @DeleteMapping("/user/{userId}")
+    // Update user by ID or add new one
+    @PostMapping("")
+    public void saveUserById(@RequestParam(value="userId", required=false) Long userId,
+                             @RequestParam String fullName,
+                             @RequestParam String email,
+                             @RequestParam String password,
+                             @RequestParam String phoneNumber,
+                             @RequestParam Date dob,
+                             @RequestParam String gender) {
+        userService.saveUserById(userId, fullName, email, password, phoneNumber, dob, gender);
+    }
+
+    // Delete user by ID
+    @DeleteMapping("/{userId}")
     public void deleteUserById(@PathVariable("userId") Long userId) { userService.deleteUserById(userId); }
-
-    @PutMapping("/user/{userId}")
-    public void updateUserById(@RequestBody AppUser user) {
-        userService.updateUserById(user);
-    }
-
-    // get all users with pagination and sort
-    @GetMapping(path="/users/find/{page}/{category}")
-    public List<AppUser> sortAndPagination(@PathVariable("page") int currPage, @PathVariable("category") String category){
-        return userService.sortAndPagination(currPage,category);
-    }
-
-    // get all users with name
-    @GetMapping(path="/users/findByName/{page}/{name}")
-    public List<AppUser> findUsersByName(@PathVariable("page") int currPage,@PathVariable("name") String name){
-        return userService.findUsersByName(currPage,name);
-    }
-
-    // get all users with phone number
-    @GetMapping(path="/users/findByPhone/{page}/{phone}")
-    public List<AppUser> findUsersByPhone(@PathVariable("page") int currPage,@PathVariable("phone") String phone){
-        return userService.findUsersByPhoneNumber(currPage,phone);
-    }
-
-    // get all users with email
-    @GetMapping(path="/users/findByName/{page}/{email}")
-    public List<AppUser> findUsersByEmail(@PathVariable("page") int currPage,@PathVariable("email") String email){
-        return userService.findUsersByEmail(currPage,email);
-    }
 }
