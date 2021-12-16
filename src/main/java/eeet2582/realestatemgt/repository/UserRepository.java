c74260be3bb68d2e8a24106724387e64957e2f43
mnskgrayce <mineskgrayce@gmail.com>
2021-12-16 23:48:44 +0700

    refactor search with params for user, all endpoints work now

--- src/main/java/eeet2582/realestatemgt/repository/UserRepository.java
+++ src/main/java/eeet2582/realestatemgt/repository/UserRepository.java
@@ -1,23 +1,10 @@
 package eeet2582.realestatemgt.repository;
 
 import eeet2582.realestatemgt.model.AppUser;
-import eeet2582.realestatemgt.model.House;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
-    @Query("SELECT u FROM AppUser u WHERE u.fullName LIKE %?1%")
-    List<AppUser> findAppUsersByName(String name, Pageable pageable);
-
-    @Query("SELECT u FROM AppUser u WHERE u.email LIKE %?1%")
-    List<AppUser> findAppUsersByEmail(String email, Pageable pageable);
-
-    @Query("SELECT u FROM AppUser u WHERE u.phoneNumber = ?1")
-    List<AppUser> findAppUsersByPhoneNumber(String phoneNumber, Pageable pageable);
 }
--- src/main/java/eeet2582/realestatemgt/service/UserService.java
+++ src/main/java/eeet2582/realestatemgt/service/UserService.java
@@ -3,12 +3,11 @@ package eeet2582.realestatemgt.service;
 import eeet2582.realestatemgt.model.AppUser;
 import eeet2582.realestatemgt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 // UserService only wires UserRepository, everything else is handled by child services
@@ -34,48 +33,63 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    // Find users matching by full name, email or phone number
+    public Page<AppUser> getFilteredUsers(String query, int pageNo, int pageSize, String sortBy, String orderBy) {
+        AppUser user = new AppUser();
+        user.setFullName(query);
+        user.setEmail(query);
+        user.setPhoneNumber(query);
+
+        ExampleMatcher matcher = ExampleMatcher.matchingAny()
+                .withMatcher("fullName", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
+                .withMatcher("email", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
+                .withMatcher("phoneNumber", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+        Example<AppUser> example= Example.of(user, matcher);
+
+        Pageable pageable;
+        if (orderBy.equals("asc")) {
+            pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).ascending());
+        } else {
+            pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
+        }
+        return userRepository.findAll(example, pageable);
+    }
+
+    // Get one by ID, try to reuse the exception
     public AppUser getUserById(Long userId) {
         return userRepository
                 .findById(userId)
                 .orElseThrow(() -> new IllegalStateException("User with userId=" + userId + " does not exist!"));
     }
 
+    // Transactional means "all or nothing", if the transaction fails midway nothing is saved
+    @Transactional
+    public void saveUserById(Long userId, String fullName, String email, String password, String phoneNumber, Date dob, String gender) {
+        // If ID is provided, try to find the current user, else make new one
+        AppUser user = (userId != null)? getUserById(userId) : new AppUser();
+
+        // Do input checking here
+
+        // Save the cleaned user
+        user.setFullName(fullName);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setPhoneNumber(phoneNumber);
+        user.setDob(dob);
+        user.setGender(gender);
+        userRepository.save(user);
+    }
+
     @Transactional
     public void deleteUserById(Long userId) {
-        if (!userRepository.existsById(userId))
-            throw new IllegalStateException("User with userId=" + userId + " does not exist!");
+        AppUser user = getUserById(userId);
 
         // Delete all classes that depend on current user
         adminService.deleteDepositsByUserId(userId);
         adminService.deleteMeetingsByUserId(userId);
         rentalService.deleteRentalsByUserId(userId);
 
-        userRepository.deleteById(userId);
+        // Finally, delete the user
+        userRepository.delete(user);
     }
-
-    @Transactional
-    public void updateUserById(AppUser user) {
-        userRepository.save(user);
-    }
-
-    public List<AppUser> sortAndPagination(int currPage, String category){
-        Pageable page = PageRequest.of(currPage, 10, Sort.by(category).descending()); // 10 users per page
-        return userRepository.findAll(page).getContent();
-    }
-
-    public List<AppUser> findUsersByName(int currPage,String name){
-        Pageable pageable = PageRequest.of(currPage,10,Sort.by("name").descending());
-        return userRepository.findAppUsersByName(name,pageable);
-    }
-
-    public List<AppUser> findUsersByEmail(int currPage,String email){
-        Pageable pageable = PageRequest.of(currPage,10,Sort.by("name").descending());
-        return userRepository.findAppUsersByEmail(email,pageable);
-    }
-
-    public List<AppUser> findUsersByPhoneNumber(int currPage,String phoneNumber){
-        Pageable pageable = PageRequest.of(currPage,10,Sort.by("name").descending());
-        return userRepository.findAppUsersByPhoneNumber(phoneNumber,pageable);
-    }
-
 }
