4834d4b1bf8974f97ee52811b6905b79c72a7042
mnskgrayce <mineskgrayce@gmail.com>
2022-01-18 12:43:30 +0700

    reset cache and delete unneeded files

--- src/main/java/eeet2582/realestatemgt/model/helper/EStatus.java
+++
@@ -1,7 +0,0 @@
-package eeet2582.realestatemgt.model.helper;
-
-public enum EStatus {
-    AVAILABLE,
-    RESERVED,
-    RENTED
-}
--- src/main/java/eeet2582/realestatemgt/model/helper/EType.java
+++
@@ -1,6 +0,0 @@
-package eeet2582.realestatemgt.model.helper;
-
-public enum EType {
-    APARTMENT,
-    STREET
-}
